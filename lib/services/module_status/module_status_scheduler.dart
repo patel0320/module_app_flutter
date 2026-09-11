ce30d13b0aadf08602385bb91c95c4e91773062a
devcraft <devcraft.0320@gmail.com>
2026-09-11 07:26:58 +0200

    module_status_scheduler onLifecycleCHanged

--- lib/services/module_status/module_status_scheduler.dart
+++ lib/services/module_status/module_status_scheduler.dart
@@ -55,6 +55,8 @@ class ModuleStatusScheduler with WidgetsBindingObserver {
   }
 
   void _onLifecycleChanged(AppLifecycleState state) {
+    debugPrint('ModuleStatusScheduler: app backgrounded ($_state) => ($state)');
+
     if (state == _state) return;
     _state = state;
     if (state == AppLifecycleState.resumed) {
